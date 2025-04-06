3515a541e1bca0f558c83d1301024f8b5c035cb7
Mohit Kambli <mohitkambli8@gmail.com>
2025-04-06 17:24:27 -0400

    Task-2: Setting up Model & Repository

---
+++ src/main/java/com/consultadd/SpringBootAssignment/SpringBootAssignment/model/Item.java
@@ -0,0 +1,50 @@
+package com.consultadd.SpringBootAssignment.SpringBootAssignment.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Item() {}
+
+    public Item(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Column(nullable = false)
+    private Double price;
+
+}
