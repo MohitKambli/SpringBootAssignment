d7cd488a487d467e98a8131d89149a6dd27caa79
Mohit Kambli <mohitkambli8@gmail.com>
2025-04-06 18:49:36 -0400

    Cleaning the project

--- src/main/java/com/consultadd/SpringBootAssignment/SpringBootAssignment/service/impl/ItemServiceImpl.java
+++ src/main/java/com/consultadd/SpringBootAssignment/SpringBootAssignment/service/impl/ItemServiceImpl.java
@@ -5,7 +5,6 @@ import com.consultadd.SpringBootAssignment.SpringBootAssignment.repository.ItemR
 import com.consultadd.SpringBootAssignment.SpringBootAssignment.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
 
